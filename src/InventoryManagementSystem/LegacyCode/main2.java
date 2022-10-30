8c85ce0757c0cd65bd703f042846a4b53fce391a
Jakob Calvert <jakobcalvert@gmail.com>
2022-10-30 19:33:20 +1300

    v1.2.5 added comments

--- src/InventoryManagementSystem/LegacyCode/main2.java
+++ src/InventoryManagementSystem/LegacyCode/main2.java
@@ -6,7 +6,6 @@ package InventoryManagementSystem.LegacyCode;
 
 import InventoryManagementSystem.AllStock;
 import InventoryManagementSystem.LegacyCode.StaffMode;
-import InventoryManagementSystem.SaveToFile;
 import java.util.Scanner;
 
 /**
--- src/InventoryManagementSystem/ServerSaveFile.java
+++ src/InventoryManagementSystem/ServerSaveFile.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
  *
  * @author Jakob
  */
+//this class is resposible for saving and reading from the server
 public class ServerSaveFile {
 
     DataBaseManager dbManager;
@@ -27,8 +28,8 @@ public class ServerSaveFile {
         conn = dbManager.getConnection();
         try {
             statement = conn.createStatement();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
     }
@@ -36,7 +37,7 @@ public class ServerSaveFile {
     //initialises the tables for the inventory management system if not already done will not be called unless the program cannot read a table
     public void initialiseTables() {
         try {
-
+            //each batch is a sql statement to create a table the two tables priced by weight and priced by unit are dependant on the table stores
             statement.addBatch("CREATE TABLE STORES (\n"
                     + "Name VARCHAR(256),\n"
                     + "PRIMARY KEY (Name)\n"
@@ -50,7 +51,7 @@ public class ServerSaveFile {
                     + "PRIMARY KEY (ITEMNAME),\n"
                     + "NAME VARCHAR(256)"
                     + ")");
-
+            //adds foreign key to the stores table
             statement.addBatch("alter table pricedbyweight add FOREIGN KEY (NAME) REFERENCES STORES(NAME)");
 
             statement.addBatch("CREATE TABLE pricedByunit (\n"
@@ -62,8 +63,10 @@ public class ServerSaveFile {
                     + "PRIMARY KEY (ITEMNAME),\n"
                     + "NAME VARCHAR(256)"
                     + ")");
+            //adds foreign key to the stores table
             statement.addBatch("alter table pricedbyunit add FOREIGN KEY (NAME) REFERENCES STORES(NAME)");
 
+            //execute all batch statements
             statement.executeBatch();
 
         } catch (SQLException ex) {
@@ -71,6 +74,7 @@ public class ServerSaveFile {
         }
     }
 
+    //this method adds a new priced by unit table entry into the database
     public void addPricedByUnit(Inventory e, PricedByUnit item) {
         try {
             statement.addBatch("INSERT INTO pricedbyunit VALUES ('" + item.getAmount() + "', '" + item.getStockPrice() + "', '" + item.getPrice() + "', '" + item.getWeight() + "', '" + item.getName() + "', '" + e.getLocationName() + "')");
@@ -80,6 +84,7 @@ public class ServerSaveFile {
         }
     }
 
+    //this method adds a new priced by weight entry into the data base
     public void addPricedByWeight(Inventory e, PricedByWeight item) {
         try {
             statement.addBatch("INSERT INTO pricedbyweight VALUES ('" + item.getAmountKg() + "', '" + item.getStockPrice() + "', '" + item.getPricePerKg() + "', '" + item.getName() + "', '" + e.getLocationName() + "')");
